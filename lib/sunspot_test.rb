b5343f8772f770cae2b82a0ae24b8216633aa95c
Ryan G <ersatzryan@gmail.com>
2011-05-30 22:47:19 -0400

    Moves functionality into module and refactores sunspottest/cucumber to use that module so that functionality can be easily called in other test frameworks
    Refactors wait_until_solr_starts to move the actual ping call to its own method (solr_running?) and adds tests for both.
    
    Fixes sunspot_test/cucumber had changed module name and forgot to change it back.
    
    Fixes gemspec file, changes solr_ping_url from constant to evaluated before ping. This was causing some issues, so easy solution to just evaluated it.

---
+++ lib/sunspot_test.rb
@@ -0,0 +1,78 @@
+require 'sunspot/rails'
+require 'net/http'
+
+module SunspotTest
+  class TimeOutError < StandardError; end;
+  class << self
+
+    attr_writer :solr_startup_timeout
+    attr_writer :server
+
+    def solr_startup_timeout
+      @solr_startup_timeout || 15
+    end
+
+    def setup_solr
+      unstub
+      start_sunspot_server
+    end
+
+    def server
+      @server ||= Sunspot::Rails::Server.new
+    end
+
+    def start_sunspot_server
+      unless @server
+        pid = fork do
+          STDERR.reopen("/dev/null")
+          STDOUT.reopen("/dev/null")
+          server.run
+        end
+
+        at_exit { Process.kill("TERM", pid) }
+
+        wait_until_solr_starts
+      end
+    end
+
+    # Stubs Sunspot calls to Solr server
+    def stub
+      unless @session_stubbed
+        Sunspot.session = Sunspot::Rails::StubSessionProxy.new(original_sunspot_session)
+        @session_stubbed = true
+      end
+    end
+
+    # Resets Sunspot to call Solr server, opposite of stub
+    def unstub
+      if @session_stubbed
+        Sunspot.session = original_sunspot_session
+        @session_stubbed = false
+      end
+    end
+
+    private
+
+    def original_sunspot_session
+      @original_sunspot_session ||= Sunspot.session
+    end
+
+    def wait_until_solr_starts
+      (solr_startup_timeout * 10).times do
+        break if solr_running?
+        sleep(0.1)
+      end
+      raise TimeOutError, "Solr failed to start after #{solr_startup_timeout} seconds" unless solr_running?
+    end
+
+    def solr_running?
+      begin
+        solr_ping_uri = URI.parse("#{Sunspot.session.config.solr.url}/ping")
+        Net::HTTP.get(solr_ping_uri)
+        true # Solr Running
+      rescue
+        false # Solr Not Running
+      end
+    end
+  end
+end
--- spec/sunspot_test_spec.rb
+++ spec/sunspot_test_spec.rb
@@ -1,4 +1,160 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "SunspotTest" do
+describe SunspotTest do
+  describe ".solr_startup_timeout" do
+    after(:all) { SunspotTest.solr_startup_timeout = 15 }
+    it "defaults to 15" do
+      SunspotTest.solr_startup_timeout.should eq(15)
+    end
+
+    it "can be set using .solr_startup_timeout=" do
+      SunspotTest.solr_startup_timeout = 20
+      SunspotTest.solr_startup_timeout.should eq(20)
+    end
+  end
+
+  describe "setup_solr" do
+    it "calls unstub and start_sunspot_server" do
+      SunspotTest.should_receive(:unstub)
+      SunspotTest.should_receive(:start_sunspot_server)
+      SunspotTest.setup_solr
+    end
+  end
+
+  describe "server" do
+    it "creates a new Sunspot Rails Server" do
+      Sunspot::Rails::Server.should_receive(:new)
+      SunspotTest.server
+    end
+  end
+
+  describe ".start_sunspot_server" do
+    context "if server is already started" do
+      before(:each) { SunspotTest.server = true }
+
+      it "does not try to spin up another server" do
+        Kernel.should_not_receive(:fork)
+        SunspotTest.start_sunspot_server
+      end
+    end
+
+    context "if the server has NOT been started" do
+      before(:all) do
+        SunspotTest.stub!(:wait_until_solr_starts) { true }
+      end
+
+      before(:each) do
+        SunspotTest.server = nil
+
+        fake_server = mock("server")
+        SunspotTest.stub!(:server) { fake_server }
+      end
+
+      # was getting funky issues when the test was broken up
+      it "forks the process, redirects stdout/stderr, runs server, sets exit hook, waits for server" do
+
+        SunspotTest.should_receive(:fork) do |&block|
+          STDERR.should_receive(:reopen).with("/dev/null")
+          STDOUT.should_receive(:reopen).with("/dev/null")
+          SunspotTest.server.should_receive(:run)
+          block.call
+        end
+
+        SunspotTest.should_receive(:at_exit) do |&block|
+          Process.should_receive(:kill)
+          block.call
+        end
+
+        SunspotTest.should_receive(:wait_until_solr_starts)
+        SunspotTest.start_sunspot_server
+      end
+    end
+
+    describe ".stub" do
+      context "if the session is already stubbed" do
+        before(:each) { SunspotTest.instance_variable_set(:@session_stubbed, true) }
+
+        it "does nothing" do
+          Sunspot::Rails::StubSessionProxy.should_not_receive(:new)
+
+          SunspotTest.stub
+        end
+      end
+
+      context "if the session is not currently stubbed" do
+        before(:each) { SunspotTest.instance_variable_set(:@session_stubbed, false) }
+
+        it "stubs the Sunspot session and sets @session_stubbed to true" do
+          Sunspot::Rails::StubSessionProxy.should_receive(:new)
+
+          SunspotTest.stub
+        end
+
+        it "sets @session_stubbed to true" do
+          SunspotTest.stub
+          SunspotTest.instance_variable_get(:@session_stubbed).should eq(true)
+        end
+      end
+    end
+
+    describe ".unstub" do
+      context "if the session is not stubbed" do
+        before(:each) { SunspotTest.instance_variable_set(:@session_stubbed, false) }
+        it "does nothing" do
+          SunspotTest.should_not_receive(:original_sunspot_session)
+          SunspotTest.unstub
+        end
+      end
+
+      context "if the session is currently stubbed" do
+        before(:each) { SunspotTest.instance_variable_set(:@session_stubbed, true) }
+
+        it "sets the Sunspot session to the original session" do
+          SunspotTest.should_receive(:original_sunspot_session)
+
+          SunspotTest.unstub
+        end
+
+        it "sets @session_stubbed to true" do
+          SunspotTest.unstub
+          SunspotTest.instance_variable_get(:@session_stubbed).should eq(false)
+        end
+      end
+    end
+
+    describe ".solr_running" do
+      context "if solr is running" do
+        before(:each) { Net::HTTP.stub!(:get).and_return(true) }
+
+        it "returns true" do
+          SunspotTest.send(:solr_running?).should eq(true)
+        end
+      end
+
+      context "if solr is not running" do
+        it "returns false" do
+          SunspotTest.send(:solr_running?).should eq(false)
+        end
+      end
+    end
+    describe ".wait_until_solr_starts" do
+      context "if solr never starts" do
+        before(:each) { SunspotTest.solr_startup_timeout = 2 }
+
+        it "calls solr_running? until solr_startup_timeout raises TimeOutError" do
+          SunspotTest.should_receive(:solr_running?).exactly((SunspotTest.solr_startup_timeout * 10) + 1).times
+
+          lambda { SunspotTest.send(:wait_until_solr_starts) }.should raise_error SunspotTest::TimeOutError, "Solr failed to start after #{SunspotTest.solr_startup_timeout} seconds"
+        end
+      end
+
+      context "if solr is started" do
+        it "calls solr_running? and returns" do
+          SunspotTest.should_receive(:solr_running?).exactly(2).times.and_return(true)
+
+          lambda { SunspotTest.send(:wait_until_solr_starts) }.should_not raise_error
+        end
+      end
+    end
+  end
 end
