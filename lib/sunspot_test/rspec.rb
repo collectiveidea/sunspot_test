89a9ae7167e2c15a22738d77fa6e8812a161e473
Aaron Jensen <aaronjensen@gmail.com>
2013-08-14 23:33:43 -0700

    Use before :each instead of before :all for rspec
    As it was, solr was not clearing between specs. Also, if you one spec that required search and another that didn't within the same block you would search when you do not want it depending on the order the specs are run in.

--- lib/sunspot_test/rspec.rb
+++ lib/sunspot_test/rspec.rb
@@ -1,11 +1,11 @@
 require 'sunspot_test'
 
 RSpec.configure do |c|
-  c.before(:all) do
+  c.before(:each) do
     SunspotTest.stub
   end
   
-  c.before(:all, :search => true) do
+  c.before(:each, :search => true) do
     SunspotTest.setup_solr
     Sunspot.remove_all!
     Sunspot.commit
