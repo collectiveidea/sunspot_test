e1a7e4f485cac0640c9179b410a8317e324f8753
Ryan Glover <ersatzryan@gmail.com>
2014-03-28 23:37:52 -0400

    Add code climate coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
